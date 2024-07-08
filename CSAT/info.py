927b15bce8f4f8c374f81b8515911b2cae8dfed9
Fahad Kabali <31004638+fahadkabali@users.noreply.github.com>
2024-07-08 15:59:18 +0300

    Update info.py
    U

--- CSAT/info.py
+++ CSAT/info.py
@@ -1,6 +1,1 @@
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER= 'kabalifahad@gmail.com'
-EMAIL_HOST_PASSWORD= 'coim hgba oxzg hpng' #password generated from security page on manage account section of that email account(search for "App Password" and generate new for your custom app)
-EMAIL_PORT = 587
