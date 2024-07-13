253cd66af284e6a420bf33354502d5e97de65fc9
fahadkabali <kabalifahad@gmail.com>
2024-07-13 23:40:00 +0300

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('password_reset/', views.ResetPasswordView.as_view(), name='password_reset'),
     path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),name='password_reset_confirm'),
     path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),name='password_reset_complete'),
+    # path('password-change/', ChangePasswordView.as_view(), name='password_change'),
+
 
     # path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
     # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
