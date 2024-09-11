90c0edde55220d3fe3cb76abf2cefba7fe436741
fahadkabali <kabalifahad@gmail.com>
2024-09-11 20:55:14 +0300

    update

--- home/forms.py
+++ home/forms.py
@@ -1,62 +1,27 @@
-# from django import forms
-# from .models import Question, Choice, UserResponse
-
-# class ResponseForm(forms.ModelForm):
-#     class Meta:
-#         model = UserResponse
-#         fields = ['selected_choices']
-#         widgets = {
-#             'selected_choices': forms.CheckboxSelectMultiple,
-#         }
-
-#     def __init__(self, *args, **kwargs):
-#         question = kwargs.pop('question')
-#         super().__init__(*args, **kwargs)
-#         if question.question_type == Question.SINGLE_CHOICE:
-#             self.fields['selected_choices'].widget = forms.RadioSelect()
-#             self.fields['selected_choices'].queryset = question.choices.all()
-#         else:
-#             question.question_type == Question.MULTIPLE_CHOICE
-#             self.fields['selected_choices'].queryset = question.choices.all()
-
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         if self.instance.question.question_type in [Question.SINGLE_CHOICE, Question.MULTIPLE_CHOICE]:
-#             if not cleaned_data.get('selected_choices'):
-#                 raise forms.ValidationError('You must select at least one choice.')
-#         return cleaned_data
-
-# forms.py
-
 from django import forms
 from .models import Question, Choice, UserResponse
 
-class ResponseForm(forms.Form):
+class ResponseForm(forms.ModelForm):
+    class Meta:
+        model = UserResponse
+        fields = ['selected_choices']
+        widgets = {
+            'selected_choices': forms.CheckboxSelectMultiple,
+        }
+
     def __init__(self, *args, **kwargs):
+        question = kwargs.pop('question')
         super().__init__(*args, **kwargs)
-        for question in Question.objects.prefetch_related('choices'):
-            if question.question_type == Question.SINGLE_CHOICE:
-                self.fields[f'question_{question.id}'] = forms.ModelChoiceField(
-                    queryset=question.choices.all(),
-                    widget=forms.RadioSelect,
-                    required=True
-                )
-            else:
-                self.fields[f'question_{question.id}'] = forms.ModelMultipleChoiceField(
-                    queryset=question.choices.all(),
-                    widget=forms.CheckboxSelectMultiple,
-                    required=False
-                )
-    
-    def save_response(self, user, question):
-        field_name = f'question_{question.id}'
-        if field_name in self.cleaned_data:
-            choices = self.cleaned_data[field_name]
-            if choices:
-                response = UserResponse.objects.create(user=user, question=question)
-                if isinstance(choices, Choice):  # Single choice
-                    response.selected_choices.add(choices)
-                else:  # Multiple choices
-                    response.selected_choices.add(*choices)
-                return response
-        return None
+        if question.question_type == Question.SINGLE_CHOICE:
+            self.fields['selected_choices'].widget = forms.RadioSelect()
+            self.fields['selected_choices'].queryset = question.choices.all()
+        else:
+            question.question_type == Question.MULTIPLE_CHOICE
+            self.fields['selected_choices'].queryset = question.choices.all()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if self.instance.question.question_type in [Question.SINGLE_CHOICE, Question.MULTIPLE_CHOICE]:
+            if not cleaned_data.get('selected_choices'):
+                raise forms.ValidationError('You must select at least one choice.')
+        return cleaned_data
--- home/utils.py
+++ home/utils.py
@@ -1,53 +1,63 @@
-# utils.py
-
+from django.conf import settings
+from PIL import Image, ImageDraw, ImageFont
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from reportlab.lib.utils import ImageReader
 from io import BytesIO
-from PIL import Image, ImageDraw, ImageFont
 import os
-from django.conf import settings
-from .models import UserResponse, Question
+
+def calculate_normalized_score(total_score, max_possible_score):
+    return round(min(100, (total_score / max_possible_score) * 100), 1)
+
+def get_result_data(normalized_score):
+    if normalized_score > 80:
+        return {'text': 'Advanced', 'color': '#32EE32', 'class': 'success'}
+    elif 60 < normalized_score <= 80:
+        return {'text': 'Average', 'color': '#004400', 'class': 'primary'}
+    elif 40 < normalized_score <= 60:
+        return {'text': 'Basic', 'color': '#FFA400', 'class': 'warning'}
+    else:
+        return {'text': 'Weak', 'color': '#FF0000', 'class': 'danger'}
 
 def generate_certificate_pdf(user, normalized_score, result):
-    # Create a BytesIO buffer for the PDF
-    buffer = BytesIO()
-
-    # Create the PDF object, using the BytesIO buffer as its "file."
-    c = canvas.Canvas(buffer, pagesize=letter)
-
-    # Load the certificate template
     template_path = os.path.join(settings.STATIC_ROOT, 'milima.png')
-    c.drawImage(template_path, 0, 0, width=letter[0], height=letter[1])
+    certificate = Image.open(template_path)
+    draw = ImageDraw.Draw(certificate)
 
-    # Set font and size for text
-    c.setFont("Helvetica-Bold", 24)
+    font_path = os.path.join(settings.STATIC_ROOT, 'open-sans.bold.ttf')
+    font = ImageFont.truetype(font_path, 60)
 
-    # Add user information
-    c.drawString(200, 400, f"Name: {user.get_full_name()}")
-    c.drawString(200, 370, f"Email: {user.email}")
-    c.drawString(200, 340, f"Organization: {user.organisation_name}")
+    coordinates = {
+        'fullname': (890, 1520),
+        'email': (890, 1590),
+        'organisation_name': (890, 1665),
+        'total_score': (2164, 2190),
+        'score_text': (1500, 2035),
+        'score_circle': (688, 2096)
+    }
 
-    # Add score information
-    c.drawString(200, 280, f"Score: {normalized_score}%")
-    c.drawString(200, 250, f"Result: {result['text']}")
+    draw.text(coordinates['fullname'], user.fullname, fill="black", font=font)
+    draw.text(coordinates['email'], user.email, fill="black", font=font)
+    draw.text(coordinates['organisation_name'], user.organisation_name, fill="black", font=font)
+    draw.text(coordinates['total_score'], f"{normalized_score} %", fill=result['color'], font=font)
+    draw.text(coordinates['score_text'], result['text'], fill=result['color'], font=font)
 
-    # Add assessment details
-    c.setFont("Helvetica", 12)
-    y_position = 200
-    questions = Question.objects.all()
-    for question in questions:
-        response = UserResponse.objects.filter(user=user, question=question).first()
-        if response:
-            c.drawString(100, y_position, f"Q: {question.text}")
-            y_position -= 20
-            c.drawString(120, y_position, f"A: {', '.join(choice.text for choice in response.selected_choices.all())}")
-            y_position -= 30
+    circle_center = coordinates['score_circle']
+    circle_radius = 135
+    draw.ellipse(
+        (circle_center[0] - circle_radius, circle_center[1] - circle_radius,
+         circle_center[0] + circle_radius, circle_center[1] + circle_radius),
+        fill=result['color']
+    )
 
-    # Save the PDF
+    image_buffer = BytesIO()
+    certificate.save(image_buffer, format='PNG')
+    image_buffer.seek(0)
+
+    pdf_buffer = BytesIO()
+    c = canvas.Canvas(pdf_buffer, pagesize=letter)
+    c.drawImage(ImageReader(image_buffer), 0, 0, width=letter[0], height=letter[1])
     c.showPage()
     c.save()
 
-    # Get the value of the BytesIO buffer and return it
-    pdf = buffer.getvalue()
-    buffer.close()
-    return pdf
+    return pdf_buffer.getvalue()
